f1434e49e448fddc7489d030480883a097dbb3bf
lucaszhao <hubukizhao@gmail.com>
2018-12-25 00:47:23 +0800

    merry christmas

--- SignIn/config.py
+++ SignIn/config.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 DEBUG = True
 PORT = 5000
 HOST = "localhost"
@@ -6,6 +8,7 @@ SECRET_KEY = "signin"
 JWT_AUTH_URL_RULE = '/login'
 JWT_AUTH_USERNAME_KEY = 'phonenum'
 JWT_AUTH_PASSWORD_KEY = 'password'
+JWT_EXPIRATION_DELTA = timedelta(seconds=600)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
